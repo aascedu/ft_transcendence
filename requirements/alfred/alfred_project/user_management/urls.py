a6f9b5a062883ca73cdec7aa5c642f96809b84d5
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-03-29 10:48:13 +0100

    style:

--- requirements/alfred/alfred_project/user_management/urls.py
+++ requirements/alfred/alfred_project/user_management/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from user_management.views import view_db, createUser, userInfoView, friendView
 
 urlpatterns = [
-        path("users/<int:id>", userInfoView.as_view()),
-        path("friends/<int:id>", friendView.as_view()),
-        path("test", createUser),
-        path("view-db", view_db)
+    path("users/<int:id>", userInfoView.as_view()),
+    path("friends/<int:id>", friendView.as_view()),
+    path("test", createUser),
+    path("view-db", view_db)
 ]
