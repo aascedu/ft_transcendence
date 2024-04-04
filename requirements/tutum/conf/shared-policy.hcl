fc2ac223ee2b708a09040fa56d25289bd9ea8620
Arthur Ascedu <aascedu@z3r6p1.42lyon.fr>
2024-04-04 16:47:16 +0200

    build(tutum): now called shared token stored in shared_token.py

---
+++ requirements/tutum/conf/shared-policy.hcl
@@ -0,0 +1,3 @@
+path "secret/data/shared/*" {
+  capabilities = [ "read" ]
+}
--- requirements/tutum/tools/script.sh
+++ requirements/tutum/tools/script.sh
@@ -13,14 +13,14 @@ if [ $? -eq 2 ]; then
 
     # Create policy for tokens to be created on
     vault policy write env /env-policy.hcl
-    vault policy write petrus /petrus-policy.hcl
+    vault policy write shared /shared-policy.hcl
 
-    # Create and write in .py file the token for petrus and in .txt for other containers
-    mkdir -p tokens/petrus
+    # Create and write in .py file the token for shared and in .txt for other containers
+    mkdir -p tokens/shared
     mkdir -p tokens/davinci
-    echo -n 'vault_token = "' >> /tokens/petrus/petrus-token.py
-    vault token create -policy=petrus | grep 'token' | awk '{print $2}' | head -n 1 | tr -d '\n' >> /tokens/petrus/petrus-token.py
-    echo -n '"' >> /tokens/petrus/petrus-token.py
+    echo -n 'vault_token = "' >> /tokens/shared/shared_token.py
+    vault token create -policy=shared | grep 'token' | awk '{print $2}' | head -n 1 | tr -d '\n' >> /tokens/shared/shared_token.py
+    echo -n '"' >> /tokens/shared/shared_token.py
     vault token create -policy=env | grep 'token' | awk '{print $2}' | head -n 1 > /tokens/env-token.txt
     cp /tokens/env-token.txt /tokens/davinci/env-token.txt
 
@@ -30,9 +30,9 @@ if [ $? -eq 2 ]; then
     vault kv put -mount=secret env/epw EPW=$ELASTIC_PASSWORD
     vault kv put -mount=secret env/gpw GPW=$GRAFANA_PASSWD
     priv=`openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:2048`
-    vault kv put -mount=secret petrus/priv private_key="$priv"
+    vault kv put -mount=secret shared/priv private_key="$priv"
     pub=$(echo "$priv" | openssl rsa -pubout -outform PEM -in /dev/stdin)
-    vault kv put -mount=secret petrus/pub public_key="$pub"
+    vault kv put -mount=secret shared/pub public_key="$pub"
 else
     KEY=`cat /tokens/tutum.txt | head -n 1`
     vault operator unseal $KEY
