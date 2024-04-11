1fa855af6128bf794ff6eb18eb5a82dfd7af197e
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-04-11 13:42:12 +0200

    fix: add migrations to script.sh

--- requirements/petrus/tools/script.sh
+++ requirements/petrus/tools/script.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 python manage.py makemigrations
+python manage.py makemigrations signin
 python manage.py migrate
+python manage.py migrate signin
 python manage.py runserver 0.0.0.0:8009
 # gunicorn --bind 0.0.0.0:8009 petrus_project.wsgi:application
