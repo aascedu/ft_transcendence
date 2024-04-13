b901a8dc67399b94b113fd2d5483b571015b1e91
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-04-13 16:54:54 +0200

    feat: set admin urls

--- requirements/petrus/petrus_project/petrus_project/urls.py
+++ requirements/petrus/petrus_project/petrus_project/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
     path('auth/', include("signin.urls")),
     path('metrics/', include('django_prometheus.urls')),
+    path('admin/', admin.site.urls),
+
+
 ]
