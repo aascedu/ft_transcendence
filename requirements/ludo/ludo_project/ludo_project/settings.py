e18b58e809fb645e259e7cafead51ebe1fbf8a7b
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-04-14 12:57:05 +0200

    clean(Ludo): admin

--- requirements/ludo/ludo_project/ludo_project/settings.py
+++ requirements/ludo/ludo_project/ludo_project/settings.py
@@ -35,12 +35,6 @@ INSTALLED_APPS = [
     'daphne',
     'pong',
     'channels',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [] + shared_middleware
