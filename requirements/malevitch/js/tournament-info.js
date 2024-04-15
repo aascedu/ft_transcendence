0d9c317430efaab8bc690b71b5bbebb0caec1fa4
Charlie Crottier-Combe <ccrottie@z3r6p2.42lyon.fr>
2024-04-15 16:33:06 +0200

    feat (malevitch): edit tournament - kick a player

--- requirements/malevitch/js/tournament-info.js
+++ requirements/malevitch/js/tournament-info.js
@@ -45,7 +45,7 @@ document.querySelector('.tournament-info-leave-icon').addEventListener('click', 
 	document.querySelector('.tournament-info-leave-alert').classList.remove('visually-hidden');
 });
 
-	// Confirm / cancel the remove
+	// Confirm / cancel the leaving
 
 document.querySelector('.tournament-info-leave-alert .alert-confirm-button').addEventListener('click', function () {
 	document.querySelector('.tournament-info-leave-alert').classList.add('visually-hidden');
@@ -56,4 +56,76 @@ document.querySelector('.tournament-info-leave-alert .alert-confirm-button').add
 
 document.querySelector('.tournament-info-leave-alert .alert-cancel-button').addEventListener('click', function () {
 	document.querySelector('.tournament-info-leave-alert').classList.add('visually-hidden');
+});
+
+// Enter edit mode
+
+document.querySelector('.tournament-info-edit-icon').addEventListener('click', function () {
+	// Switch button appearance
+	document.querySelector('.tournament-info-edit-icon').classList.add('visually-hidden');
+	document.querySelector('.tournament-info-check-icon').classList.remove('visually-hidden');
+
+	// Reveal kick buttons
+	document.querySelectorAll('.tournament-kick-player').forEach(item => {
+		item.classList.remove('visually-hidden');
+	});
+
+	// TODO : reveal edit for tournament name
+});
+
+// Kick a player
+
+document.querySelectorAll('.tournament-kick-player').forEach(function(item) {
+	item.addEventListener('click', function(event) {
+		event.stopPropagation();
+
+		// Show alert
+		document.querySelector('.tournament-info-kick-alert').classList.remove('visually-hidden');
+
+		// Confirm the kick
+		document.querySelector('.tournament-info-kick-alert .alert-confirm-button').addEventListener('click', function() {
+			// Hide alert
+			document.querySelector('.tournament-info-kick-alert').classList.add('visually-hidden');
+
+			var playerToHide = item.parentNode;
+
+			playerToHide.parentNode.removeChild(playerToHide);
+		});
+
+		// Cancel the kick
+		document.querySelector('.tournament-info-kick-alert .alert-cancel-button').addEventListener('click', function() {
+			// Hide alert
+			document.querySelector('.tournament-info-kick-alert').classList.add('visually-hidden');
+		});
+	});
+});
+
+// Leave edit mode
+
+document.querySelector('.tournament-info-check-icon').addEventListener('click', function () {
+	// Show alert
+	document.querySelector('.tournament-info-edit-alert').classList.remove('visually-hidden');
+});
+
+	// Confirm / cancel the leaving
+
+document.querySelector('.tournament-info-edit-alert .alert-cancel-button').addEventListener('click', function () {
+	// Hide alert
+	document.querySelector('.tournament-info-edit-alert').classList.add('visually-hidden');
+});
+
+document.querySelector('.tournament-info-edit-alert .alert-confirm-button').addEventListener('click', function () {
+	// Hide alert
+	document.querySelector('.tournament-info-edit-alert').classList.add('visually-hidden');
+
+	// Switch button appearance
+	document.querySelector('.tournament-info-check-icon').classList.add('visually-hidden');
+	document.querySelector('.tournament-info-edit-icon').classList.remove('visually-hidden');
+
+	// Hide kick buttons
+	document.querySelectorAll('.tournament-kick-player').forEach(item => {
+		item.classList.add('visually-hidden');
+	});
+
+	// TODO : hide edit for tournament name
 });
