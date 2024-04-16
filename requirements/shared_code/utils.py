b8df4e79d4618acad25fec2bf6aa4c1598493793
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-04-16 14:58:14 +0200

    feat: add delete_response to shared code

--- requirements/shared_code/utils.py
+++ requirements/shared_code/utils.py
@@ -1,3 +1,5 @@
+from django.db.models import ProtectedError
+from django.db.models.lookups import In
 from django.http import JsonResponse
 from django.db import IntegrityError
 from django.core.exceptions import ValidationError
@@ -6,10 +8,23 @@ from django.core.exceptions import ValidationError
 def save_response(object_to_save):
     try:
         object_to_save.save()
+        return JsonResponse({"Ressource": "updated"})
     except IntegrityError as e:
         return JsonResponse({"Err", e.__str__()}, status=409)
     except ValidationError as e:
         return JsonResponse({"Err", e.__str__()}, status=422)
     except BaseException as e:
         return JsonResponse({"Err", e.__str__()}, status=500)
-    return JsonResponse({"Client": "updated"})
+
+
+def delete_response(object_to_delete):
+    try:
+        object_to_delete.delete()
+        return JsonResponse({"Ressource": "deleted"})
+    except ProtectedError as e:
+        return JsonResponse({"Err", e.__str__()}, status=422)
+    except IntegrityError as e:
+        return JsonResponse({"Err", e.__str__()}, status=409)
+    except BaseException as e:
+        return JsonResponse({"Err", e.__str__()}, status=500)
+
