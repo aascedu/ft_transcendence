1a10c263257461cba4738c574e3c49bd71d865f3
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-04-22 16:21:09 +0200

    fix: Condition migrations to MAKE_MIGRATIONS env variable

--- requirements/alfred/tools/script.sh
+++ requirements/alfred/tools/script.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ -z "${MAKE_MIGRATIONS}" ]; then
+    echo "MAKE_MIGRATIONS is set"
+    echo "Making migrations"
+    python manage.py makemigrations
+    python manage.py makemigrations user_management
+fi
+
 python manage.py migrate
 python manage.py migrate user_management
 python manage.py runserver 0.0.0.0:8001
--- requirements/petrus/tools/script.sh
+++ requirements/petrus/tools/script.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -z "${MAKE_MIGRATIONS}" ]; then
+    echo "MAKE_MIGRATIONS is set"
+    python manage.py makemigrations
+    python manage.py makemigrations signin
+fi
 python manage.py migrate
 python manage.py migrate signin
 python manage.py runserver 0.0.0.0:8009
