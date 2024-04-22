20fb9ef31e37707b7307c5105b73f3eba7fd8399
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-04-22 13:19:06 +0200

    feat: add is_admin and is_service to User and header to User method

--- requirements/shared_code/common_classes.py
+++ requirements/shared_code/common_classes.py
@@ -1,14 +1,22 @@
+from collections.abc import KeysView
+
+
 class User:
     nick: str
     error: str
     is_autenticated: bool
+    is_admin: bool
+    is_service: bool
     id: int
 
-    def __init__(self, nick='', error='', is_autenticated=False, id=-1):
+    def __init__(self, nick='', error='', is_admin=False, is_service=False, is_autenticated=False, id=-1):
         self.nick = nick
         self.error = error
         self.is_autenticated = is_autenticated
+        self.is_admin = False
+        self.is_service = False
         self.id = id
+
 
     def __str__(self):
         return f"{self.nick}, {self.id}, {self.is_autenticated}\n"
@@ -19,3 +27,13 @@ class User:
             'error': self.error,
             'id': self.id,
         }
+
+    @staticmethod
+    def header_to_user(header):
+        try:
+            return User(nick=header['Service_Name'], is_service=True)
+        except KeyError:
+            print("Error: Internal request badly Formated 'Service_Name' key misses")
+            return User(error="Bad Internal request : 'Service_Name' key missing")
+
+
