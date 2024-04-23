bf616aa35360be11671714e5818b0709720c8e73
Arthur Ascedu <aascedu@z3r6p1.42lyon.fr>
2024-04-23 15:06:11 +0200

    clean: init script for postgres exporters

--- requirements/mensura/db_exporter_a/tools/entrypoint.sh
+++ requirements/mensura/db_exporter_a/tools/entrypoint.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 
-#sleep 1000
 DATA_SOURCE_NAME="postgresql://prom:prom1@alfred_db:5432/user_management?sslmode=disable" postgres_exporter --web.listen-address=:9187 --web.telemetry-path=/metrics
