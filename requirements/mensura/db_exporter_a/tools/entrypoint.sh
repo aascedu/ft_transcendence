b27c954a72b297a9a6033bd70463d04f49d3d088
Arthur Ascedu <aascedu@z3r6p1.42lyon.fr>
2024-04-30 11:30:27 +0200

    clean: no more hardcoded passwd

--- requirements/mensura/db_exporter_a/tools/entrypoint.sh
+++ requirements/mensura/db_exporter_a/tools/entrypoint.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
-
+TOKEN=$(cat /tokens/db_exporter-token.txt)
+export PROM_PASSWD=$(curl -s --header "X-Vault-Token: $TOKEN" http://tutum:8200/v1/secret/data/env/pg_monitor | jq -r '.data.data.pg_monitor')
+echo a $PROM_PASSWD
 DATA_SOURCE_NAME="postgresql://prom:prom1@alfred_db:5432/user_management?sslmode=disable" postgres_exporter --web.listen-address=:9187 --web.telemetry-path=/metrics --collector.database
