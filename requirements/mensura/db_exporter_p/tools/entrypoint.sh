600f18d44dcd987cec6f7e32c04f6a6c972b51e0
Arthur Ascedu <aascedu@z3r6p1.42lyon.fr>
2024-04-30 12:05:56 +0200

    clean: debug echo removed

--- requirements/mensura/db_exporter_p/tools/entrypoint.sh
+++ requirements/mensura/db_exporter_p/tools/entrypoint.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
 TOKEN=$(cat /tokens/db_exporter-token.txt)
 export PROM_PASSWD=$(curl -s --header "X-Vault-Token: $TOKEN" http://tutum:8200/v1/secret/data/env/pg_monitor | jq -r '.data.data.pg_monitor')
-echo p $PROM_PASSWD
 DATA_SOURCE_NAME="postgresql://prom:prom1@petrus_db:5432/auth_management?sslmode=disable" postgres_exporter --web.listen-address=:9187 --web.telemetry-path=/metrics --collector.database
