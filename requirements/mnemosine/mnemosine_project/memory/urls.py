88b28a6d02c6c43a67f7ad90084f7a557be26ec0
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-05-06 14:22:20 +0200

    cleaning: suppress viewdb

--- requirements/mnemosine/mnemosine_project/memory/urls.py
+++ requirements/mnemosine/mnemosine_project/memory/urls.py
@@ -1,4 +1,3 @@
-from django.http import JsonResponse
 from django.urls import path
 from memory.views import (
         tournamentView,
@@ -6,22 +5,7 @@ from memory.views import (
         playerView,
 )
 
-from memory.models import Player, Game
-
-def view_db(request):
-    return JsonResponse({"Id": request.user.id, "Players":
-                    [e.to_dict() for e in Player.objects.all()]
-                         })
-
-
-def view_game_db(request):
-    return JsonResponse({"Games":
-        [e.to_dict() for e in Game.objects.all()]
-    })
-
 urlpatterns = [
-    path("view-db", view_db),
-    path("view-game-db", view_game_db),
     path("pong/tournaments", tournamentView.as_view()),
     path("pong/games", gameView.as_view()),
     path("players/<int:id>", playerView.as_view()),
