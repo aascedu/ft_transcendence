0990ca6b1eb39ad69bf2814bc872233313e39c76
hgeffroy <henri.geffroy@gmail.com>
2024-05-15 14:27:26 +0200

    style(malevitch): fetch couberting preparation

---
+++ requirements/malevitch/js/fetch-coubertin.js
@@ -0,0 +1,10 @@
+function add_coubertin_in_url(url) {
+    return ("/coubertin/tournament/" + url)
+}
+
+// Functions:
+// Join a tournament
+// Remove player from a tournament
+// Create a tournament
+// Get list of tournaments you can enter
+// Get one tournament infos
