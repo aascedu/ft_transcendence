0f5f8ccb56a25717537f333fe7e046e25ebb6683
Charlie <43385667+CharlieCCln@users.noreply.github.com>
2024-05-16 16:55:50 +0200

    Update fetch_mnemosine.js

--- requirements/malevitch/js/fetch_mnemosine.js
+++ requirements/malevitch/js/fetch_mnemosine.js
@@ -1,11 +1,11 @@
 function add_mnemosine_to_url(url) {
-    return ("/mnemosine/memory" + url)
+    return ("/mnemosine/memory" + url);
 }
 
 function get_game_history(id) {
-    return fetch_get(add_mnemosine_to_url("/pong/players/" + id))
+    return fetch_get(add_mnemosine_to_url("/pong/players/" + id));
 }
 
 function get_tournament_history(id) {
-    return fetch_get(add_mnemosine_to_url("/pong/tournaments/" + id))
+    return fetch_get(add_mnemosine_to_url("/pong/tournaments/" + id));
 }
