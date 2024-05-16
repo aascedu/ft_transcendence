ec64bc2e4eab4c8bceaed29cedea2d55805edefc
hgeffroy <henri.geffroy@gmail.com>
2024-05-16 10:41:22 +0200

    build(malevitch): basic fetch to tournament

--- requirements/malevitch/js/fetch-coubertin.js
+++ requirements/malevitch/js/fetch-coubertin.js
@@ -3,8 +3,90 @@ function add_coubertin_in_url(url) {
 }
 
 // Functions:
-// Join a tournament
-// Remove player from a tournament
-// Create a tournament
-// Get list of tournaments you can enter
-// Get one tournament infos
+
+async function join_tournament(id) {
+    json = JSON.stringify({Id: id});
+    method = 'POST';
+    headers = {'Content-Type': 'application/json',};
+
+    return fetch(add_coubertin_in_url('/entry/'),
+                {
+                    json: json,
+                    method: method,
+                    headers: headers,
+                }
+            )
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+}
+
+async function remove_player_from_tournament(id) {
+    json = JSON.stringify({Id: id});
+    method = 'PATCH';
+    headers = {'Content-Type': 'application/json',};
+
+    return fetch(add_coubertin_in_url('/entry/'),
+                {
+                    json: json,
+                    method: method,
+                    headers: headers,
+                }
+            )
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+}
+
+// Peut-etre admin a add ici.
+async function create_tournament(tournamentName, nbPlayers) {
+    json = JSON.stringify({tournamentName: tournamentName, nbPlayers: nbPlayers});
+    method = 'POST';
+    headers = {'Content-Type': 'application/json',};
+    
+    return fetch(add_coubertin_in_url('/infos/'),
+                {
+                    json: json,
+                    method: method,
+                    headers: headers,
+                }
+            )
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+}
+
+async function get_tournaments_available() {
+    json = JSON.stringify({id: -1});
+    method = 'GET';
+    headers = {'Content-Type': 'application/json',};
+
+    return fetch(add_coubertin_in_url('/infos/'),
+                {
+                    json: json,
+                    method: method,
+                    headers: headers,
+                }
+            )
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+}
+
+async function get_tournament_infos(tournamentId) {
+    json = JSON.stringify({id: tournamentId});
+    method = 'GET';
+    headers = {'Content-Type': 'application/json',};
+
+    return fetch(add_coubertin_in_url('/infos/'),
+                {
+                    json: json,
+                    method: method,
+                    headers: headers,
+                }
+            )
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error));
+}
+
