0201461dd0c197fe6b75baedea1e520ed4d50ccc
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-05-22 16:39:12 +0200

    feat: shared response

--- requirements/shared_code/utils.py
+++ requirements/shared_code/utils.py
@@ -26,20 +26,20 @@ def delete_response(request, object_to_delete):
         return JsonResponseLogging(request, {"Err": e.__str__()}, status=409)
     return JsonResponseLogging(request, {"Ressource": "deleted"})
 
-def JsonErrResponse(error_message, status):
-    return JsonResponseLogging({"Err": error_message}, status)
+def JsonErrResponse(request, error_message, status):
+    return JsonResponseLogging(request, {"Err": error_message}, status)
 
-def JsonUnauthorized(error_message):
-    return JsonErrResponse("Unauthorized : " + error_message, status=401)
+def JsonUnauthorized(request, error_message):
+    return JsonErrResponse(request, "Unauthorized : " + error_message, status=401)
 
-def JsonBadRequest(error_message):
-    return JsonErrResponse("Bad Request : " + error_message, status=400)
+def JsonBadRequest(request, error_message):
+    return JsonErrResponse(request, "Bad Request : " + error_message, status=400)
 
-def JsonForbiden(error_message):
-    return JsonErrResponse("Forbiden : " + error_message, status=403)
+def JsonForbiden(request, error_message):
+    return JsonErrResponse(request, "Forbiden : " + error_message, status=403)
 
-def JsonNotFound(error_message):
-    return JsonErrResponse("Ressource not found : " + error_message, status=404)
+def JsonNotFound(request, error_message):
+    return JsonErrResponse(request, "Ressource not found : " + error_message, status=404)
 
-def JsonConflict(error_message):
-    return JsonErrResponse("Conflict : " + error_message, status=409)
+def JsonConflict(request, error_message):
+    return JsonErrResponse(request, "Conflict : " + error_message, status=409)
