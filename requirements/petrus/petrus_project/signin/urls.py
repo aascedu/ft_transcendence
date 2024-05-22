a3115738759ce52bee38d73d9c97fb179c5c6485
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-05-22 17:07:38 +0200

    feat: response petrus

--- requirements/petrus/petrus_project/signin/urls.py
+++ requirements/petrus/petrus_project/signin/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from signin.views import signinView, signupView, refreshView, view_db
+from signin.views import signinView, signupView, refreshView
 
 
 urlpatterns = [
-    path("view-db", view_db),
     path("signin/<str:string>", signinView.as_view()),
     path("signup", signupView.as_view()),
     path("JWT-refresh", refreshView.as_view()),
