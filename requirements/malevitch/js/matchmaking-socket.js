0af84fec741768f74345a628f90d7a2b0f1bc2e0
hgeffroy <henri.geffroy@gmail.com>
2024-05-22 14:03:51 +0200

    build(malevitch): init socket functions

--- requirements/malevitch/js/matchmaking-socket.js
+++ requirements/malevitch/js/matchmaking-socket.js
@@ -1,55 +1,56 @@
-/***************************************** Websockets *****************************************/
-
-const exampleSocket = new WebSocket('wss://localhost:8000/cupidon/matchmaking/ws/');
-const Id = g_userId;
-
-exampleSocket.onopen = function(event) {
-    console.log("Mathcmaking socket opened in the front");
-    sendPlayerData("playerData");
-};
-
-exampleSocket.onclose = function() {
-    console.log("Matchmaking socket closed in the front");
-    sendData("Leaving");
-}
-
-exampleSocket.onerror = function(event) {
-    console.log("Socket error");
-}
-
-exampleSocket.onmessage = (event) => {
-    const data = JSON.parse(event.data);
-    if (data.action === "redirect") {
-        window.location.href = data.url; // NON (Pas SPA)
+async function init_matchmaking_socket() {
+    const exampleSocket = new WebSocket('wss://localhost:8000/cupidon/matchmaking/ws/');
+    const Id = g_userId;
+    
+    exampleSocket.onopen = function(event) {
+        console.log("Mathcmaking socket opened in the front");
+        sendPlayerData("playerData");
+    };
+    
+    exampleSocket.onclose = function() {
+        console.log("Matchmaking socket closed in the front");
+        sendData("Leaving");
     }
-};
-
-console.log(Id)
-
-function sendPlayerData(type) {
-    // Construct a msg object containing the data the server needs to process the message from the chat client.
-    const data = {
-        type: type,
-        id: Id, // A recuperer depuis la db
-        elo: 500, // A recuperer depuis la db, le back va le recuperer
+    
+    exampleSocket.onerror = function(event) {
+        console.log("Socket error");
+    }
+    
+    exampleSocket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        if (data.action === "redirect") {
+            window.location.href = data.url; // NON (Pas SPA)
+        }
     };
-  
-    // Send the msg object as a JSON-formatted string.
-    exampleSocket.send(JSON.stringify(data));
+    
+    console.log(Id)
+    
+    function sendPlayerData(type) {
+        // Construct a msg object containing the data the server needs to process the message from the chat client.
+        const data = {
+            type: type,
+            id: Id, // A recuperer depuis la db
+            elo: 500, // A recuperer depuis la db, le back va le recuperer
+        };
+      
+        // Send the msg object as a JSON-formatted string.
+        exampleSocket.send(JSON.stringify(data));
+    }
+    
+    function sendData(type) {
+        const data = {
+            type: type,
+        };
+    
+        exampleSocket.send(JSON.stringify(data));
+    }
+    
+    function ping() {
+        sendData('ping');
+    }
+    
+    exampleSocket.addEventListener('open', (event) => {
+        const intervalID = setInterval(ping, 10000); // Juste envoyer sendData('ping') ?
+    });
 }
 
-function sendData(type) {
-    const data = {
-        type: type,
-    };
-
-    exampleSocket.send(JSON.stringify(data));
-}
-
-function ping() {
-    sendData('ping');
-}
-
-exampleSocket.addEventListener('open', (event) => {
-    const intervalID = setInterval(ping, 10000); // Juste envoyer sendData('ping') ?
-});
--- requirements/malevitch/js/notif-socket.js
+++ requirements/malevitch/js/notif-socket.js
@@ -1,31 +1,32 @@
-const userName = "pierrepierrepierre"
-const url = 'wss://localhost:8000/hermes/session/' + userName + '/'
-const notificationSocket = new WebSocket(url); // Probably add room name
-
-notificationSocket.onopen = function(event) {
-    console.log("Hermes socket opened in the front");
-};
-
-notificationSocket.onclose = function() {
-    console.log("Hermes socket closed in the front");
+async function init_notif_socket() {
+    const url = 'wss://localhost:8000/hermes/session/' + g_userId + '/'
+    const notificationSocket = new WebSocket(url); // Probably add room name
+    
+    notificationSocket.onopen = function(event) {
+        console.log("Hermes socket opened in the front");
+    };
+    
+    notificationSocket.onclose = function() {
+        console.log("Hermes socket closed in the front");
+    }
+    
+    notificationSocket.onerror = function(event) {
+        console.log("Socket error");
+    }
+    
+    notificationSocket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+    
+        if (data.type === "notification.message") {
+            msg = data.message;
+            // Faire pop la notif
+        }
+    
+        if (data.type === "start.game") {
+            roomName = toString(data.player1) + '-' + toString(data.player2) + '/';
+            init_socket(roomName);
+            // Cacher ce qu'il faut pour lancer la game
+        }
+    };
 }
 
-notificationSocket.onerror = function(event) {
-    console.log("Socket error");
-}
-
-notificationSocket.onmessage = (event) => {
-    const data = JSON.parse(event.data);
-
-    if (data.type === "notification.message") {
-        msg = data.message;
-        // Faire pop la notif
-    }
-
-    if (data.type === "start.game") {
-        roomName = toString(data.player1) + '-' + toString(data.player2) + '/';
-        init_socket(roomName);
-        // Cacher ce qu'il faut pour lancer la game
-    }
-
-};
