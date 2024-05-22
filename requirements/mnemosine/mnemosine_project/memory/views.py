b98c8106660714b1660edafb433dcd8ad00525c9
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-05-22 17:03:42 +0200

    feat: memory use logging request

--- requirements/mnemosine/mnemosine_project/memory/views.py
+++ requirements/mnemosine/mnemosine_project/memory/views.py
@@ -5,86 +5,87 @@ from django.http import JsonResponse
 from django.views import View
 
 from memory.models import Tournament, Game, Player
+from shared.utils import JsonResponseLogging as JsonResponse
 from shared.utils import JsonNotFound, JsonUnauthorized, delete_response, save_response, JsonBadRequest, JsonErrResponse, JsonForbiden
 
 class tournamentHistoryView(View):
     def get(self, request, id: int):
         if request.user.is_autenticated is False:
-            return JsonUnauthorized("Connect yourself to fetch this data")
+            return JsonUnauthorized(request, 'Connect yourself to fetch this data')
         try:
             player = Player.objects.get(id=id)
         except ObjectDoesNotExist:
-            JsonNotFound("Id is not found in db")
+            JsonNotFound(request, 'Id is not found in db')
         return_json = {player.id: [tournament.to_dict() for tournament in player.tournaments.all()]}
 
-        return JsonResponse(return_json)
+        return JsonResponse(request, return_json)
 
 class tournamentView(View):
     def get(self, request, id: int):
         if request.user.is_autenticated is False:
-            return JsonUnauthorized("Connect yourself to fetch this data")
+            return JsonUnauthorized(request, 'Connect yourself to fetch this data')
         try:
             tournament = Tournament.objects.get(id=id)
         except ObjectDoesNotExist as e:
-            return JsonNotFound(f'Tournament can\'t be found for this id : {e}')
-        return JsonResponse(tournament.to_dict())
+            return JsonNotFound(request, f'Tournament can\'t be found for this id : {e}')
+        return JsonResponse(request, tournament.to_dict())
 
     def post(self, request, id:int  = 0):
         if request.user.is_service is False:
-            return JsonForbiden("Only Coubertin can create tournaments")
+            return JsonForbiden(request, 'Only Coubertin can create tournaments')
         data = request.data
         try:
             Tournament.from_json_saved(data)
         except IntegrityError as e:
-            return JsonErrResponse(e.__str__(), status=409)
+            return JsonErrResponse(request, e.__str__(), status=409)
         except ValidationError as e:
-            return JsonErrResponse(e.__str__(), status=422)
-        return JsonResponse({"Ressource": "updated"})
+            return JsonErrResponse(request, e.__str__(), status=422)
+        return JsonResponse(request, {'Ressource': 'updated'})
 
 
 class gameView(View):
     def post(self, request):
         if request.user.is_service is False:
-            return JsonForbiden("Only services can post games")
+            return JsonForbiden(request, "Only services can post games")
         try:
             new_game = Game.from_json_saved(request.data)
             new_game.game_db_update()
         except IntegrityError as e:
-            return JsonErrResponse(e.__str__(), status=409)
+            return JsonErrResponse(request, e.__str__(), status=409)
         except ValidationError as e:
-            return JsonErrResponse(e.__str__(), status=422)
+            return JsonErrResponse(request, e.__str__(), status=422)
         except BaseException as e:
-            return JsonErrResponse(e.__str__(), status=500)
-        return JsonResponse(new_game.to_dict())
+            return JsonErrResponse(request, e.__str__(), status=500)
+        return JsonResponse(request, new_game.to_dict())
 
     def delete(self, request):
         if request.user.is_admin is False:
-            return JsonForbiden("Only admin can delete games")
-        return JsonErrResponse("Not implemented delete", status=501)
+            return JsonForbiden(request, "Only admin can delete games")
+        return JsonErrResponse(request, "Not implemented delete", status=501)
 
 
 class playerView(View):
     def get(self, request, id: int = 0):
         if request.user.is_autenticated is False:
-            return JsonUnauthorized("Connect yourself to get history")
+            return JsonUnauthorized(request, "Connect yourself to get history")
         try:
             player = Player.objects.get(id=id)
             return_json = player.to_dict()
             return_json |= {'History': [game.to_dict() for game in Game.objects.filter(Q(winner=player) | Q(loser=player)).order_by('-id')[:15]]}
         except ObjectDoesNotExist:
-            return JsonNotFound("Player is not found")
+            return JsonNotFound(request, f'Player {id} is not found')
 
-        return JsonResponse(return_json)
+        return JsonResponse(request, return_json)
 
     def post(self, request, id: int = 0):
         if request.user.is_service is False:
-            return JsonErrResponse("Only services can create a player", status=401)
+            return JsonForbiden(request, "Only services can create a player")
         player = Player()
         try:
             player.id = request.data['Id']
         except KeyError as e:
-            return JsonBadRequest(f"Key : {str(e)} not provided.")
-        return save_response(player)
+            return JsonBadRequest(request, f'Key : {str(e)} not provided.')
+        return save_response(request, player)
 
     def delete(self, request, id: int = 0):
         if request.user.is_service is False \
@@ -93,31 +94,31 @@ class playerView(View):
         try:
             id = request.data['Id']
         except KeyError as e:
-            return JsonBadRequest(f"misses value for key : {e.__str__()}")
+            return JsonBadRequest(request, f'misses value for key : {e.__str__()}')
         try:
             player = Player.objects.get(id=id)
         except ObjectDoesNotExist:
-            return JsonErrResponse("no player for the id", status=404)
+            return JsonNotFound(request, 'no player found for the id')
         return delete_response(player)
 
     def patch(self, request, id: int = 0):
         if request.user.is_admin is False:
-            return JsonErrResponse("Only admin can patch a player", status=401)
+            return JsonForbiden(request, 'Only admin can patch a player')
         try:
             id = request.data['Id']
             elo = request.data['Elo']
         except KeyError as e:
-            return JsonBadRequest(f"misses value for key : {e.__str__()}")
+            return JsonBadRequest(request, f'misses value for key : {e.__str__()}')
 
         try:
             id = int(id)
             elo = int(elo)
         except (ValueError, TypeError):
-            return JsonBadRequest(f"{id} is not a valid id")
+            return JsonBadRequest(request, f'{id} is not a valid id')
 
         try:
             player = Player.objects.get(id=id)
         except ObjectDoesNotExist:
-            return JsonErrResponse("no player for the id", status=404)
+            return JsonNotFound(request, 'no player for the id')
         player.elo = elo
         return save_response(player)
