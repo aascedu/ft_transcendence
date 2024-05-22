75e44347355608fefd97074f678fe21184906308
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-05-22 15:52:46 +0200

    feat: JsonResponse into coubertin

--- requirements/coubertin/coubertin_project/tournament/views.py
+++ requirements/coubertin/coubertin_project/tournament/views.py
@@ -1,59 +1,63 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 from django.views import View
 from tournament.classes.Tournament import Tournament, tournaments
-import logging
-import requests
+from shared.utils import JsonResponseLogging as JsonResponse, JsonUnauthorized, JsonBadRequest, JsonNotFound
+
 # Invite someone to tournament
 # Online friends not yet subscribed to tournament: avec Brieuc
 # Get matches
 
 class tournamentManagement(View): # Faire un patch pour modif le nb de joueurs ou autre ?
+    def get(self, request):
+        if request.user.is_autenticated is False:
+            return JsonUnauthorized(request, "Connect yourself to fetch")
+        data = request.data
+        if 'id' not in data:
+            return JsonBadRequest(request, {'Err': "tournament id not provided"})
+
+        if data['id'] == -1: # Pour avoir tous les tournois auxquels on peut s'inscrire
+            response = {}
+            for id in tournaments:
+                if tournaments[id].state == 0:
+                    response[id] = tournaments[id].toFront()
+            return JsonResponse(request, response)
+        else:
+            return JsonResponse(request, tournaments[data['id']].toFront())
+
     def post(self, request): # Maybe we can set admin here instead.
         global tournaments
+        if request.user.is_autenticated is False:
+            return JsonUnauthorized(request, 'Only authentified players can post tournaments')
 
         data = request.data
-        if 'Name' not in data or 'NumPlayers' not in data or 'Invited' not in data or 'Admin':
-            return JsonResponse({'Err': "missing informations to create tournament"})
-        tournamentName = data['Name']
-        nbPlayers = data['NumPlayers']
-        admin = data['Admin']
-        invited = data['Invited']
+        try:
+            tournamentName = data['Name']
+            nbPlayers = data['NumPlayers']
+            admin = data['Admin']
+            invited = data['Invited']
+        except (KeyError, TypeError, ValueError) as e:
+            return JsonBadRequest(request, {'Err': f'missing {e} to create tournament'})
 
         id = 0
         for id in tournaments:
             id += 1
 
         tournaments[id] = Tournament(tournamentName, nbPlayers, id, admin, invited) # 0 = admin id to get
-        
+
         # Faire une requete a Hermes pour inviter les gens
 
-        return JsonResponse({'Msg': "Tournament created"}) # Redirect on the tournament url, or join URL ?
-    
-    def get(self, request):
-        data = request.data
-        if 'id' not in data:
-            return JsonResponse({'Err': "tournament id not provided"})
-        
-        if data['id'] == -1: # Pour avoir tous les tournois auxquels on peut s'inscrire
-            response = {}
-            for id in tournaments:
-                if tournaments[id].state == 0:
-                    response[id] = tournaments[id].toFront()
-            return JsonResponse(response)
-        else:
-            return JsonResponse(tournaments[id].toFront())
-        
+        return JsonResponse(request, {'Msg': "Tournament created"}) # Redirect on the tournament url, or join URL ?
+
     def patch(self, request):
+        if request.user.is_admin is False:
+            return JsonUnauthorized(request, 'Only admin can patch ongoing tournaments')
         global tournaments
-
         data = request.data
-        if 'NewName' not in data or 'TournamentId' not in data:
-            return JsonResponse({'Err': "missing informations to change tournament name"})
-        
-        tournaments[data['TournamentId']].name = data['NewName']
-
-        return JsonResponse({'Msg': "Tournament name changed"})
+        try:
+            tournaments[data['TournamentId']].name = data['NewName']
+        except KeyError as e:
+            return JsonBadRequest(request, f'missing key {e}')
+        return JsonResponse(request, {'Msg': "Tournament name changed"})
 
 class tournamentEntry(View):
     def patch(self, request): # Leave
@@ -64,58 +68,61 @@ class tournamentEntry(View):
             data = request.data
             tournamentId = data['TournamentId']
             tournaments[tournamentId].removePlayer(playerId)
-            
-        except Exception as e:
-            return JsonResponse({'Err': e.__str__()})
-        
-        # Deconnecter la websocket
+        except KeyError as e:
+            return JsonBadRequest(request, f'missing key {e}')
+        return JsonResponse(request, {"Tournament": "Patched"})
 
-        return JsonResponse({})
-    
-    def post(self, request): # Join
+    def post(self, request): #Join
         global tournaments
-        
-        try:
-            playerId = request.user.id
-            data = request.data
-            TournamentId = data['TournamentId']
-            if (TournamentId not in tournaments):
-                return JsonResponse({'Err': "tournament does not exists"})
-            tournaments[TournamentId].addPlayer(playerId)
-            
-        except Exception as e:
-            return JsonResponse({'Err': e.__str__()})
 
-        url = 'wss://localhost:8000/coubertin/tournament/ws/' + str(TournamentId)
-        return JsonResponse({'Msg': "tournament joined", 'url': url}) # url of the websocket to join
-        
+        playerId = request.user.id
+        data = request.data
+        try:
+            TournamentId = data['TournamentId']
+        except KeyError as e:
+            return JsonBadRequest(request, f'missing key {e}')
+        try:
+            TournamentId = int(TournamentId)
+        except (TypeError, ValueError) as e:
+            return JsonBadRequest(request, f'bad content {e}')
+        if (TournamentId not in tournaments):
+            return JsonNotFound(request, "tournament does not exists")
+        tournaments[TournamentId].addPlayer(playerId)
+        return JsonResponse(request, {'Msg': "tournament joined"})
+
 class inviteFriend(View):
     def post(self, request):
+        if request.user.is_autenticated is False:
+            return JsonUnauthorized(request, 'Only authentified player can invite friends')
+
         global tournaments
 
         data = request.data
-        if 'Invited' not in data or 'TournamentId' not in data:
-            return JsonResponse({'Err': "tournament does not exists"})
-        TournamentId = data['TournamentId']
-        if (TournamentId not in tournaments):
-            return JsonResponse({'Err': "tournament does not exists"})
-        
-        tournaments[TournamentId].invited.append(data['Invited']) 
 
         try:
-            requests.post(
-                'http://tournament-request/' + str(request.user.id), 
-                json={'Tournament-Id': TournamentId,
-                        'Tournament-Name': tournaments[TournamentId].name,
-                        'Notified': data['Invited']})
-            
-        except Exception as e:
-            return JsonResponse({'Err': e.__str__()})
+            TournamentId = data['TournamentId']
+            invited = data['Invited']
+        except KeyError as e:
+            return JsonBadRequest(request, f'missing key {e}')
+        try:
+            TournamentId = int(TournamentId)
+            invited = int(invited)
+        except (TypeError, ValueError) as e:
+            return JsonBadRequest(request, f'bad content {e}')
+
+        TournamentId = data['TournamentId']
+        if (TournamentId not in tournaments):
+            return JsonNotFound(request, 'tournament does not exists')
+
+        tournaments[TournamentId].invited.append(invited)
+
+        # Send a Hermes
+        return JsonResponse
 
 class myTournaments(View):
     def get(self, request):
         userId = request.user.id
-        response = {}
+        response = []
 
         for i in tournaments:
             if tournaments[i].userParticipating(userId):
@@ -136,10 +143,6 @@ class gameResult(View): # We need to remove the loser from the player list
         printData(data)
         tournament = tournaments[data['tournamentId']]
         tournament.addGame(data['game']) # Game is a dictionnary
-
-        # Envoyer un next round si ongoingGames vaut 0
-        # Deconnecter la websocket du perdant
-
         return JsonResponse({})
 
 def tournamentHome(request, tournamentId):
@@ -148,7 +151,7 @@ def tournamentHome(request, tournamentId):
 
 ############## Debug ##############
 def printData(data):
-    print('Tournament id: ', data['tournamentId'])
+    print('Tournament name: ', data['tournamentName'])
     game = data['game']
-    print('Winner is ', game['Winner'], ' with a score of ', game['Winner-score'])
-    print('Loser is ', game['Loser'], ' with a score of ', game['Loser-score'])
+    print('Player ', game['Player1'], ' had a score of ', game['Score1'])
+    print('Player ', game['Player2'], ' had a score of ', game['Score2'])
