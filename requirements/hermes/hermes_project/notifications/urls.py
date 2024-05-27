6fcf5279a5d0a89dcfc2f1d4e1280bdae40751b1
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-05-27 16:27:23 +0200

    feat: available feature

--- requirements/hermes/hermes_project/notifications/urls.py
+++ requirements/hermes/hermes_project/notifications/urls.py
@@ -4,6 +4,7 @@ from notifications import views
 
 urlpatterns = [
     path("online-states", views.onlineView.as_view(), name="online states"),
+    path('available-states', views.availableFriendView.as_view()),
     path("send-shit/", views.global_notification, name="notifications"),
     path("friendship/<int:requester>", views.friendship),
     path("friend-request/<int:requester>", views.friendshipRequest),
