115ce6041af5283d8ed9e6f77b7e3627304ae848
hgeffroy <henri.geffroy@gmail.com>
2024-05-31 15:33:35 +0200

    fix(coubertin): removed body from delete request

--- requirements/coubertin/coubertin_project/tournament/urls.py
+++ requirements/coubertin/coubertin_project/tournament/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("available-tournaments/", views.availableTournamentView.as_view(), name="available tournament"),
     path("infos/<int:id>/", views.tournamentManagement.as_view(), name="createTournament"),
-    path("entry/", views.tournamentEntry.as_view(), name="entry"),
+    path("entry/<int: tournamentId>/<int: playerId>/", views.tournamentEntry.as_view(), name="entry"),
     path("invite/", views.inviteFriend.as_view(), name="invite"),
     path("mytournaments/", views.myTournaments.as_view(), name="myTournaments"),
     path("gameResult/", views.gameResult.as_view(), name="gameresult"),
--- requirements/malevitch/js/fetch-coubertin.js
+++ requirements/malevitch/js/fetch-coubertin.js
@@ -15,21 +15,17 @@ async function change_tournament_name(NewName, TournamentId) {
 }
 
 async function remove_player_from_tournament(TournamentId, PlayerId) {
-    json = {TournamentId: TournamentId, PlayerId: PlayerId};
+    return fetch_delete(add_coubertin_in_url('entry/' + toString(TournamentId) + '/' + toString(PlayerId) + '/'));
+}
 
-    return fetch_delete(add_coubertin_in_url('entry/'), json);
+async function join_tournament(TournamentId) {
+    return fetch_post(add_coubertin_in_url('entry/' + toString(TournamentId) + '/0/'), json);
 }
 
 async function invite_friend_to_tournament(TournamentId, Invited) {
     json = {TournamentId: TournamentId, Invited: Invited};
 
-    return fetch_post(add_coubertin_in_url('entry/'), json);
-}
-
-async function join_tournament(TournamentId) {
-    json = {TournamentId: TournamentId};
-
-    return fetch_post(add_coubertin_in_url('entry/'), json);
+    return fetch_post(add_coubertin_in_url('invite/'), json);
 }
 
 async function get_my_tournaments() {
@@ -41,7 +37,7 @@ async function get_tournaments_available() {
 }
 
 async function get_tournament_infos(tournamentId) {
-    return fetch_get(add_coubertin_in_url('infos/' + tournamentId + '/'));
+    return fetch_get(add_coubertin_in_url('infos/' + toString(tournamentId) + '/'));
 }
 
 async function start_tournament(tournamentId) {
