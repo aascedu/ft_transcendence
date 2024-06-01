84370a28f30e64f89df9bba16c112e942dea1d62
twang <theawang@yahoo.com>
2024-06-01 10:44:30 +0200

    build( Apollo ): added ILM scripts for alias, template, policy

--- requirements/apollo/scripts/logs_alias.sh
+++ requirements/apollo/scripts/logs_alias.sh
@@ -1,7 +1,7 @@
 curl -X PUT -u elastic:elastic123 "https://localhost:9200/%3Ctimeseries_policy-index-%7Bnow%2Fd%7BYYYYMMdd%7D%7D-1%3E?pretty" -H 'Content-Type: application/json' --cacert config/certs/ca/ca.crt -d '
 {
   "aliases": {
-    "timeseries_policy-index": {
+    "test-alias": {
       "is_write_index": true
     }
   }
--- requirements/apollo/scripts/logs_template.sh
+++ requirements/apollo/scripts/logs_template.sh
@@ -1,12 +1,13 @@
 curl -X PUT -u elastic:elastic123 "https://localhost:9200/_index_template/timeseries_template?pretty" -H 'Content-Type: application/json' --cacert config/certs/ca/ca.crt -d '
 {
-  "index_patterns": ["timeseries"],
+  "index_patterns": ["timeseries-*"],
   "data_stream": { },
   "template": {
     "settings": {
       "number_of_shards": 1,
       "number_of_replicas": 1,
-      "index.lifecycle.name": "timeseries_policy"
+      "index.lifecycle.name": "timeseries_policy",
+      "index.lifecycle.rollover_alias": "test-alias"
     }
   }
 }
