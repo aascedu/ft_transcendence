3d78c6f8e65e6a5e5afdfde2d11d540e9aeb47b3
twang <theawang@yahoo.com>
2024-06-01 17:23:21 +0200

    test( ILM policy ): trying to automate ILM policy

--- requirements/apollo/scripts/logs_policy.sh
+++ requirements/apollo/scripts/logs_policy.sh
@@ -1,12 +1,38 @@
-curl -X PUT -u elastic:elastic123 "https://localhost:9200/_ilm/policy/timeseries_policy?pretty" -H 'Content-Type: application/json' --cacert config/certs/ca/ca.crt -d '
-{
+#!/bin/bash
+COLOR_GREEN='\e[1;32m'
+COLOR_RESET='\e[0m'
+
+# Define the URL for Elasticsearch
+ELASTICSEARCH_URL="https://localhost:9200"
+
+# Define the path to the CA certificate
+CA_CERT="/usr/share/elasticsearch/config/certs/ca/ca.crt"
+
+# Function to check Elasticsearch availability
+check_elasticsearch() {
+    curl --cacert "$CA_CERT" "$ELASTICSEARCH_URL" > /dev/null 2>&1
+}
+
+# Wait for Elasticsearch to become available
+until check_elasticsearch; do
+    echo -e "${COLOR_GREEN}Elasticsearch is not yet available. Waiting...${COLOR_RESET}"
+    sleep 10
+done
+
+# Once Elasticsearch is available, create the ILM policy
+echo -e "${COLOR_GREEN}Elasticsearch is now available. Configuring ILM policy...${COLOR_RESET}"
+
+curl -X PUT -u elastic:elastic123 "$ELASTICSEARCH_URL/_ilm/policy/my_policy?pretty" \
+-H 'Content-Type: application/json' \
+--cacert "$CA_CERT" \
+-d '{
   "policy": {
     "phases": {
       "hot": {
         "actions": {
           "rollover": {
             "max_primary_shard_size": "50GB",
-            "max_age": "30d"
+            "max_age": "30s"
           },
           "set_priority": {
             "priority": 50
@@ -14,7 +40,7 @@ curl -X PUT -u elastic:elastic123 "https://localhost:9200/_ilm/policy/timeseries
         }
       },
       "warm": {
-        "min_age": "7d",
+        "min_age": "7s",
         "actions": {
           "forcemerge": {
             "max_num_segments": 1
@@ -33,7 +59,7 @@ curl -X PUT -u elastic:elastic123 "https://localhost:9200/_ilm/policy/timeseries
         }
       },
       "cold": {
-        "min_age": "30d",
+        "min_age": "30s",
         "actions": {
           "set_priority": {
             "priority": 0
@@ -47,7 +73,7 @@ curl -X PUT -u elastic:elastic123 "https://localhost:9200/_ilm/policy/timeseries
         }
       },
       "delete": {
-        "min_age": "60d",
+        "min_age": "60s",
         "actions": {
           "delete": {}
         }
@@ -55,3 +81,5 @@ curl -X PUT -u elastic:elastic123 "https://localhost:9200/_ilm/policy/timeseries
     }
   }
 }'
+
+echo -e "${COLOR_GREEN}ILM policy configuration completed.${COLOR_RESET}"
--- requirements/apollo/scripts/logs_template.sh
+++ requirements/apollo/scripts/logs_template.sh
@@ -1,14 +1,27 @@
-curl -X PUT -u elastic:elastic123 "https://localhost:9200/_index_template/timeseries_template?pretty" -H 'Content-Type: application/json' --cacert config/certs/ca/ca.crt -d '
+curl -X PUT -u elastic:elastic123 "https://localhost:9200/_index_template/my_policy_template?pretty" -H 'Content-Type: application/json' --cacert config/certs/ca/ca.crt -d '
 {
-  "index_patterns": ["timeseries-*"],
-  "data_stream": { },
+  "index_patterns": ["my_policy_index-*"],
   "template": {
     "settings": {
       "number_of_shards": 1,
-      "number_of_replicas": 1,
-      "index.lifecycle.name": "timeseries_policy",
-      "index.lifecycle.rollover_alias": "test-alias"
+      "number_of_replicas": 0,
+      "index.lifecycle.name": "my_policy",
+      "index.lifecycle.rollover_alias": "my_policy_index"
+    },
+    "mappings": {
+      "properties": {
+        "name": {
+          "type": "keyword",
+          "index": true
+        },
+        "data": {
+          "type": "boolean"
+        },
+        "timestamp": {
+          "type": "date",
+          "format": "epoch_millis"
+        }
+      }
     }
   }
-}
-'
+}'
