dd422624b54681d9fc899953e911527e85e8bab9
hgeffroy <henri.geffroy@gmail.com>
2024-06-06 11:54:05 +0200

    build(cupidon): create a game from invitation

--- requirements/cupidon/cupidon_project/matchmaking/urls.py
+++ requirements/cupidon/cupidon_project/matchmaking/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path("", views.matchmaking, name="matchmaking"),
+    path('game-request/', views.RequestGame.as_view(), name='GameRequest'),
+    path('game-request-response/<int:requester>/<int:notified>/', views.RequestGameResponse.as_view(), name='GameRequestResponse'),
 ]
--- requirements/cupidon/cupidon_project/matchmaking/views.py
+++ requirements/cupidon/cupidon_project/matchmaking/views.py
@@ -1,8 +1,72 @@
-from django.shortcuts import render, redirect
+from django.views import View
+from django.shortcuts import render
+from shared.utils import JsonResponseLogging as JsonResponse, JsonUnauthorized, JsonBadRequest, JsonNotFound, JsonErrResponse
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+import requests
+import logging
 
-def matchmaking(request):
-    print(request.user.error)
-    print(request.data.get('Err', None))
-    id = request.user.id # Try catch ?
-    print("user id: ", id)
-    return render(request, "matchmaking/waitingRoom.html", {'Id': id})
+class RequestGame(View):
+    def post(self, request):
+        if request.user.is_autenticated is False:
+            return JsonUnauthorized(request, 'Only authentified player can invite friends to play')
+
+        data = request.data
+        try:
+            p1 = request.user.id
+            p2 = data['PlayerToInvite']
+        except (KeyError, TypeError, ValueError) as e:
+            return JsonBadRequest(request, {'Err': f'missing {e} to create tournament'})
+        
+        try:
+            response = requests.post(
+                'http://hermes:8004/notif/game-request/' + str(p1) + '/',
+                json={
+                    'Notified': p2,
+                }
+            )
+            if response.status_code != 200:
+                logging.warning("Failed to send invitation to player " + str(p2))
+                return JsonErrResponse(request, {'Err': "Failed to send notification to invite friend"}, status = response.status_code)
+        except Exception as e:
+            logging.error("Failed to send invitation to player " + str(p2))
+            return JsonErrResponse(request, {'Err': "Fatal: Failed to send notification to invite friend"}, status = response.status_code)
+
+        return JsonResponse(request, 'Invitation successfully sent')
+
+class RequestGameResponse(View):
+    def post(self, request, requester: int, notified: int):
+        if request.user.is_autenticated is False:
+            return JsonUnauthorized(request, 'Only authentified player can accept an invitation')
+        
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            str(requester), {
+                'type': 'SendToGame',
+                'player1': requester,
+                'player2': notified,
+            }
+        )
+
+        # Sauf si on fait a la main dans le front ?
+        async_to_sync(channel_layer.group_send)(
+            str(notified), {
+                'type': 'SendToGame',
+                'player1': requester,
+                'player2': notified,
+            }
+        )
+        # Il faut envoyer a la bonne websocket
+        # Mettre le mec unavailable pdt la recherche ?
+
+    def delete(self, request, requester: int, notified: int):
+        if request.user.is_autenticated is False:
+            return JsonUnauthorized(request, 'Only authentified player can accept an invitation')
+
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            str(requester), {
+                'type': 'Leave',
+            }
+        )
+        # Notif ?
