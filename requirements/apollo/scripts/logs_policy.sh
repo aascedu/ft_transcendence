e7fb4a235422c86ba9ab8ad9ade3aed6073db80c
Thea Wang <theawang@yahoo.com>
2024-06-16 17:42:50 +0200

    style ( logs policy ): update on the script

--- requirements/apollo/scripts/logs_policy.sh
+++ requirements/apollo/scripts/logs_policy.sh
@@ -11,263 +11,96 @@ CA_CERT="/usr/share/elasticsearch/config/certs/ca/ca.crt"
 
 set -e
 
-if [ x${ELASTIC_USER} == x ]; then
-  echo "${COLOR_RED}Set the ELASTIC_USER environment variable in the .env file${COLOR_RESET}";
-  exit 1;
-elif [ x${ELASTIC_PASSWORD} == x ]; then
-  echo "${COLOR_RED}Set the ELASTIC_PASSWORD environment variable in the .env file${COLOR_RESET}";
-  exit 1;
-fi;
-
-# Create the GeoIP ingest pipeline
-echo -e "${COLOR_GREEN}Creating GeoIP ingest pipeline...${COLOR_RESET}"
-
-curl -X PUT "$ELASTICSEARCH_URL/_ingest/pipeline/geoip" \
---cacert "$CA_CERT" \
--u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" \
--H 'Content-Type: application/json' \
--d '{
-  "description" : "Add GeoIP Info",
-  "processors" : [
-    {
-      "geoip" : {
-        "field" : "client_ip"
-      }
-    }
-  ]
-}'
-
-response=$(curl -s -X GET -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" \
-          "$ELASTICSEARCH_URL/_ingest/pipeline/geoip" \
-          -H 'Content-Type: application/json' \
-          --cacert "$CA_CERT")
-
-if [[ "$response" == *"geoip"* ]]; then
-  echo -e "${COLOR_GREEN}GeoIP pipeline created successfully.${COLOR_RESET}"
-else
-  echo -e "${COLOR_RED}Issue with GeoIP pipeline creation.${COLOR_RESET}"
+if [ -z "$ELASTIC_USER" ]; then
+  echo -e "${COLOR_RED}Set the ELASTIC_USER environment variable in the .env file${COLOR_RESET}"
+  exit 1
+elif [ -z "$ELASTIC_PASSWORD" ]; then
+  echo -e "${COLOR_RED}Set the ELASTIC_PASSWORD environment variable in the .env file${COLOR_RESET}"
   exit 1
 fi
 
-# Creating the ILM policy
-echo -e "${COLOR_GREEN}Configuring ILM policy...${COLOR_RESET}"
-
-curl -X PUT -u ${ELASTIC_USER}:${ELASTIC_PASSWORD} \
-  "$ELASTICSEARCH_URL/_ilm/policy/nginx_policy?pretty" \
-  -H 'Content-Type: application/json' \
-  --cacert "$CA_CERT" \
-  -d '{
-    "policy": {
-      "phases": {
-        "hot": {
-          "actions": {
-            "rollover": {
-              "max_primary_shard_size": "50GB",
-              "max_age": "30d"
-            },
-            "set_priority": {
-              "priority": 50
+# ILM policy JSON
+ilm_policy_json='{
+  "policy": {
+    "phases": {
+      "hot": {
+        "actions": {
+          "rollover": {
+            "max_primary_shard_size": "50GB",
+            "max_age": "30d"
+          },
+          "set_priority": {
+            "priority": 50
+          }
+        }
+      },
+      "warm": {
+        "min_age": "7d",
+        "actions": {
+          "forcemerge": {
+            "max_num_segments": 1
+          },
+          "shrink": {
+            "number_of_shards": 1
+          },
+          "allocate": {
+            "require": {
+              "data": "warm"
+            }
+          },
+          "set_priority": {
+            "priority": 25
+          }
+        }
+      },
+      "cold": {
+        "min_age": "30d",
+        "actions": {
+          "set_priority": {
+            "priority": 0
+          },
+          "freeze": {},
+          "allocate": {
+            "require": {
+              "data": "cold"
             }
           }
-        },
-        "warm": {
-          "min_age": "7d",
-          "actions": {
-            "forcemerge": {
-              "max_num_segments": 1
-            },
-            "shrink": {
-              "number_of_shards": 1
-            },
-            "allocate": {
-              "require": {
-                "data": "warm"
-              }
-            },
-            "set_priority": {
-              "priority": 25
-            }
-          }
-        },
-        "cold": {
-          "min_age": "30d",
-          "actions": {
-            "set_priority": {
-              "priority": 0
-            },
-            "freeze": {},
-            "allocate": {
-              "require": {
-                "data": "cold"
-              }
-            }
-          }
-        },
-        "delete": {
-          "min_age": "60d",
-          "actions": {
-            "delete": {}
-          }
+        }
+      },
+      "delete": {
+        "min_age": "60d",
+        "actions": {
+          "delete": {}
         }
       }
     }
-  }'
-response=$(curl -s -X GET -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" \
-          "${ELASTICSEARCH_URL}/_ilm/policy/nginx_policy?pretty" \
-          -H 'Content-Type: application/json' \
-          --cacert "${CA_CERT}")
+  }
+}'
 
-if [[ "$response" == *"nginx_policy"* ]]; then
-  echo -e "${COLOR_GREEN}Nginx policy done.${COLOR_RESET}"
-else
-  echo -e "${COLOR_RED}Issue with Nginx policy configuration.${COLOR_RESET}"
-  exit 1;
-fi
+# List of policy names
+policy_names=("nginx_policy" "logstash_policy" "filebeat_policy")
 
-curl -X PUT -u ${ELASTIC_USER}:${ELASTIC_PASSWORD} \
-  "$ELASTICSEARCH_URL/_ilm/policy/logstash_policy?pretty" \
-  -H 'Content-Type: application/json' \
-  --cacert "$CA_CERT" \
-  -d '{
-    "policy": {
-      "phases": {
-        "hot": {
-          "actions": {
-            "rollover": {
-              "max_primary_shard_size": "50GB",
-              "max_age": "30d"
-            },
-            "set_priority": {
-              "priority": 50
-            }
-          }
-        },
-        "warm": {
-          "min_age": "7d",
-          "actions": {
-            "forcemerge": {
-              "max_num_segments": 1
-            },
-            "shrink": {
-              "number_of_shards": 1
-            },
-            "allocate": {
-              "require": {
-                "data": "warm"
-              }
-            },
-            "set_priority": {
-              "priority": 25
-            }
-          }
-        },
-        "cold": {
-          "min_age": "30d",
-          "actions": {
-            "set_priority": {
-              "priority": 0
-            },
-            "freeze": {},
-            "allocate": {
-              "require": {
-                "data": "cold"
-              }
-            }
-          }
-        },
-        "delete": {
-          "min_age": "60d",
-          "actions": {
-            "delete": {}
-          }
-        }
-      }
-    }
-  }'
-response=$(curl -s -X GET -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" \
-          "${ELASTICSEARCH_URL}/_ilm/policy/logstash_policy?pretty" \
-          -H 'Content-Type: application/json' \
-          --cacert "${CA_CERT}")
+for policy_name in "${policy_names[@]}"; do
+  echo -e "${COLOR_GREEN}Configuring ILM policy for ${policy_name}...${COLOR_RESET}"
+  
+  curl -X PUT -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" \
+    "$ELASTICSEARCH_URL/_ilm/policy/${policy_name}?pretty" \
+    -H 'Content-Type: application/json' \
+    --cacert "$CA_CERT" \
+    -d "$ilm_policy_json"
+  
+  response=$(curl -s -X GET -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" \
+            "${ELASTICSEARCH_URL}/_ilm/policy/${policy_name}?pretty" \
+            -H 'Content-Type: application/json' \
+            --cacert "${CA_CERT}")
+  
+  if [[ "$response" == *"${policy_name}"* ]]; then
+    echo -e "${COLOR_GREEN}${policy_name} policy done.${COLOR_RESET}"
+  else
+    echo -e "${COLOR_RED}Issue with ${policy_name} policy configuration.${COLOR_RESET}"
+    exit 1
+  fi
+done
 
-if [[ "$response" == *"logstash_policy"* ]]; then
-  echo -e "${COLOR_GREEN}Logstash policy done.${COLOR_RESET}"
-else
-  echo -e "${COLOR_RED}Issue with Logstash policy configuration.${COLOR_RESET}"
-  exit 1;
-fi
-
-curl -X PUT -u ${ELASTIC_USER}:${ELASTIC_PASSWORD} \
-  "$ELASTICSEARCH_URL/_ilm/policy/filebeat_policy?pretty" \
-  -H 'Content-Type: application/json' \
-  --cacert "$CA_CERT" \
-  -d '{
-    "policy": {
-      "phases": {
-        "hot": {
-          "actions": {
-            "rollover": {
-              "max_primary_shard_size": "50GB",
-              "max_age": "30d"
-            },
-            "set_priority": {
-              "priority": 50
-            }
-          }
-        },
-        "warm": {
-          "min_age": "7d",
-          "actions": {
-            "forcemerge": {
-              "max_num_segments": 1
-            },
-            "shrink": {
-              "number_of_shards": 1
-            },
-            "allocate": {
-              "require": {
-                "data": "warm"
-              }
-            },
-            "set_priority": {
-              "priority": 25
-            }
-          }
-        },
-        "cold": {
-          "min_age": "30d",
-          "actions": {
-            "set_priority": {
-              "priority": 0
-            },
-            "freeze": {},
-            "allocate": {
-              "require": {
-                "data": "cold"
-              }
-            }
-          }
-        },
-        "delete": {
-          "min_age": "60d",
-          "actions": {
-            "delete": {}
-          }
-        }
-      }
-    }
-  }'
-
-response=$(curl -s -X GET -u "${ELASTIC_USER}:${ELASTIC_PASSWORD}" \
-          "${ELASTICSEARCH_URL}/_ilm/policy/filebeat_policy?pretty" \
-          -H 'Content-Type: application/json' \
-          --cacert "${CA_CERT}")
-
-if [[ "$response" == *"filebeat_policy"* ]]; then
-  echo -e "${COLOR_GREEN}Filebeat policy done.${COLOR_RESET}"
-else
-  echo -e "${COLOR_RED}Issue with Filebeat policy configuration.${COLOR_RESET}"
-  exit 1;
-fi
-
-echo -e "${COLOR_GREEN}All ILM policy configuration completed.${COLOR_RESET}"
-sleep 10
+echo -e "${COLOR_GREEN}All ILM policy configurations completed.${COLOR_RESET}"
+sleep 5
