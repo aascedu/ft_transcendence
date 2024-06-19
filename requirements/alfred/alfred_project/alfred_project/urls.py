c206f5a91f235e3bab2e05b50f03f9461471a6a4
Brieuc Poumeau de Lafforest <bpoumeau@42Lyon.fr>
2024-06-19 20:30:40 +0200

    feat: healthcheck

--- requirements/alfred/alfred_project/alfred_project/urls.py
+++ requirements/alfred/alfred_project/alfred_project/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.urls import path, include
+from shared.commonView import healthcheckView
 
 urlpatterns = [
     path('user/', include("user_management.urls")),
     path('metrics/', include('django_prometheus.urls')),
+    path('healthcheck/', healthcheckView.as_view()),
 ]
--- requirements/hermes/hermes_project/hermes_project/urls.py
+++ requirements/hermes/hermes_project/hermes_project/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
+from shared.commonView import healthcheckView
 from shared.commonView import socketConnectionView
 
 urlpatterns = [
     path('metrics/', include('django_prometheus.urls')),
     path('connectionView/', socketConnectionView.as_view()),
     path('notif/', include('notifications.urls')),
+    path('healthcheck/', healthcheckView.as_view()),
 ]
